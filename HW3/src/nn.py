6d94ca13d1b6cc0dca7bc6d420196f3bfaa6c2cd
jblamarre <77590441+jblamarre@users.noreply.github.com>
2021-03-05 22:46:12 -0500

    Update nn.py

--- HW3/src/nn.py
+++ HW3/src/nn.py
@@ -1,1 +1,21 @@
-# =============================================================================## PHYS490 - Winter 2021 - HW3 (J. Lamarre)## =============================================================================import torch.nn as nnimport torch.nn.functional as funcclass lstm_reg(nn.Module):    def __init__(self, n_dim, n_hidden, n_layers, seq_len):        super(lstm_reg, self).__init__()                self.rnn = nn.LSTM(n_dim, n_hidden, n_layers, batch_first = True)        self.fc = nn.Linear(n_hidden * seq_len, n_dim)            def forward(self, x):        x, _ = self.rnn(x)        b, s, h = x.shape        x = self.fc(x)        return x
+# =============================================================================
+#
+# PHYS490 - Winter 2021 - HW3 (J. Lamarre)
+#
+# =============================================================================
+import torch.nn as nn
+import torch.nn.functional as func
+
+#LSTM RNN with fully connected layer
+class lstm_reg(nn.Module):
+    def __init__(self, n_dim, n_hidden, n_layers, seq_len):
+        super(lstm_reg, self).__init__()
+        
+        self.rnn = nn.LSTM(n_dim, n_hidden, n_layers, batch_first = True)
+        self.fc = nn.Linear(n_hidden * seq_len, n_dim)
+        
+    def forward(self, x):
+        x, _ = self.rnn(x)
+        b, s, h = x.shape
+        x = self.fc(x)
+        return x
