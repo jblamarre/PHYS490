bb30866f25233e9cc5c99c77ef6c9a68c1210f1f
jblamarr <jblamarr@uwaterloo.ca>
2021-04-11 16:04:42 -0400

    Initial working commit for hw5

---
+++ HW5/src/data_gen.py
@@ -0,0 +1,38 @@
+# =============================================================================
+#
+# PHYS490 - Winter 2021 - HW5 (J. Lamarre) 
+#
+# =============================================================================
+import numpy as np
+from torch.utils.data import DataLoader, TensorDataset
+from torch import Tensor
+
+def batched_data(n_samples, filepath_data, batch_size, kwargs):
+    
+    #Fetch data
+    f = open(filepath_data, "r")
+    data_str = f.readlines()
+
+    n_lines = len(data_str)
+    test_samples = np.random.randint(0,n_lines, n_samples)
+
+    #Prepare blank target and 14 x 14 image arrays
+    x_train= np.zeros((n_lines,196), dtype= np.float32)
+    y_train= np.zeros((n_lines,5), dtype= np.float32)
+    
+    #Assign data to respective array
+    for c in range(n_lines):
+        data_temp = [int(s) for s in data_str[c].split(' ')]
+        y_train[c,int(data_temp.pop(-1)/2)] = 1
+        x_train[c,:] = data_temp
+    f.close()
+
+    x_train= x_train/255
+    x_test = x_train[test_samples, :]
+    y_test = y_train[test_samples, :]
+
+    data_train = DataLoader(TensorDataset(Tensor(x_train),Tensor(y_train)),
+                            batch_size = batch_size, **kwargs)
+    data_test =  DataLoader(TensorDataset(Tensor(x_test),Tensor(y_test)), **kwargs)
+    
+    return data_train,  data_test
---
+++ HW5/src/nn_gen.py
@@ -0,0 +1,48 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as func
+
+class VAE(nn.Module):
+    '''
+    Neural network class.
+    Architecture:
+
+    	Encoder:
+        	Two fully-connected layers fc1, fc2 with RelU.
+        	Compresssing layers fc31, fc32.
+
+		Decoder:
+        	Three fully-connected layers fc4, fc5, fc6 with RelU.
+    '''
+
+    def __init__(self,):
+        super(VAE, self).__init__()
+
+        self.fc1= nn.Linear(196,256)
+        self.fc2= nn.Linear(256,64)
+        self.fc31= nn.Linear(64,5)
+        self.fc32= nn.Linear(64,5)
+
+        self.fc4= nn.Linear(5,64)
+        self.fc5= nn.Linear(64,256)
+        self.fc6= nn.Linear(256,196)
+
+    def encode(self, x):
+        h= func.relu(self.fc1(x))
+        z= func.relu(self.fc2(h))
+        return self.fc31(z), self.fc32(z)
+
+    def reparameterize(self, mu, logsig):
+        std = torch.exp(0.5*logsig)
+        eps = torch.randn_like(std)
+        return mu + eps*std
+
+    def decode(self, x):
+        h= func.relu(self.fc4(x))
+        z= func.relu(self.fc5(h))
+        return torch.sigmoid(self.fc6(z))
+
+    def forward(self, x):
+        mu, logsig = self.encode(x.view(-1, 196))
+        z = self.reparameterize(mu, logsig)
+        return self.decode(z), mu, logsig
