958598738eb56d24ae7d0d9a24e2492c1413f1c3
jblamarr <jblamarr@uwaterloo.ca>
2021-04-11 22:37:29 -0400

    Comented

--- HW5/src/data_gen.py
+++ HW5/src/data_gen.py
@@ -1,11 +1,14 @@
 # =============================================================================
-#
 # PHYS490 - Winter 2021 - HW5 (J. Lamarre) 
-#
 # =============================================================================
+
+#-------------------------------Imports----------------------------------------
+
 import numpy as np
 from torch.utils.data import DataLoader, TensorDataset
 from torch import Tensor
+
+#----------------------------Data batching-------------------------------------
 
 def batched_data(n_samples, filepath_data, batch_size, kwargs):
     
@@ -13,6 +16,7 @@ def batched_data(n_samples, filepath_data, batch_size, kwargs):
     f = open(filepath_data, "r")
     data_str = f.readlines()
 
+    #Generate randome sample selection
     n_lines = len(data_str)
     test_samples = np.random.randint(0,n_lines, n_samples)
 
@@ -27,10 +31,12 @@ def batched_data(n_samples, filepath_data, batch_size, kwargs):
         x_train[c,:] = data_temp
     f.close()
 
+    #Normalize data
     x_train= x_train/255
     x_test = x_train[test_samples, :]
     y_test = y_train[test_samples, :]
 
+    #Batch data into container
     data_train = DataLoader(TensorDataset(Tensor(x_train),Tensor(y_train)),
                             batch_size = batch_size, **kwargs)
     data_test =  DataLoader(TensorDataset(Tensor(x_test),Tensor(y_test)), **kwargs)
